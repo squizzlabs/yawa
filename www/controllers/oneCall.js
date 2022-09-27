6b13b17e45cfb51d6054bf80a67054a60e058eec
cvweiss <cvweiss@gmail.com>
2022-09-27 13:23:34 -0400

    better rate limiting

--- www/controllers/oneCall.js
+++ www/controllers/oneCall.js
@@ -25,9 +25,7 @@ module.exports = {
 		}
 		req.alternativeUrl = path;
 		valid = req.verify_query_params(req, valid);
-		if (valid !== true) {
-			return valid;
-		}
+		if (valid !== true) return valid; // redirect
 
 		let result = {};
 		let isUSA = false;
@@ -35,13 +33,9 @@ module.exports = {
 		// Do we already have the lat/lon location in our database?
 		let city = await app.mysql.queryRow('select city, state_name, state_abbr from cities where lat = ? and lon = ?', [lat, lon]);
 
+		// If not, we need to look it up
 		if (city == null || city.city == undefined || city.state_name == undefined) {
-			while ((rate_limit[now] || 0) >= 1) {
-				await app.sleep(100);
-				now = app.now();
-				console.log('rate limiting at:', now);
-			}
-			rate_limit[now] = (rate_limit[now] || 0) + 1;
+			await rate_limit(app, location_rate_limiter, 1);
 
 			// Uses locationiq
 			let locationiq = 'https://us1.locationiq.com/v1/reverse.php?key=' + process.env.locationiq + '&lat=' + lat + '&lon=' + lon + '&zoom=18&format=json';
@@ -55,7 +49,6 @@ module.exports = {
 			// Double check that we're in the USA
 			if (location && location.address) {
 				isUSA = (location.address.country_code == 'us');
-				await app.redis.setex(locationiq, 864000, (isUSA ? result.location : 'Unsupported location')); // cache for 10 days
 			}
 
 			// get the state's abbreviation
@@ -71,19 +64,19 @@ module.exports = {
 
 
 		if (isUSA) {
+			await rate_limit(app, weather_rate_limiter, 1);
+
 			let openweather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + process.env.openweather;
 			let weather = await app.redis.get(epoch + ':' + openweather);
-			if (weather != null) {
-				result.weather = JSON.parse(weather);
-			} else {
-				result.weather = JSON.parse((await app.phin(openweather)).body);
-				await app.redis.setex(epoch + ':' + openweather, 900, JSON.stringify(result.weather));
+			if (weather != null) result.weather = JSON.parse(weather); // use the cached result
+			else {
+				result.weather = JSON.parse((await app.phin(openweather)).body); // fetch the weather
+				await app.redis.setex(epoch + ':' + openweather, 900, JSON.stringify(result.weather)); // and store the result in cache
 			}
 		} else {
 			result.weather = {hourly: [], daily: []};
 			result.location = 'Unsupported location';
 		}
-		
 		result.conversion = conversion;
 
 		return {
@@ -94,306 +87,107 @@ module.exports = {
 	}
 }
 
+async function rate_limit(app, object, limit_per_second) {
+	let now = Date.now();
+	let epoch = Math.floor(now / 1000).toString();
 
-
-let rate_limit = {}; // Max 1 per second, or 2 if clear_rate_limit and a poll happen in the same second, which is ok
-function clear_rate_limit() {
-	rate_limit = {};
-	setTimeout(clear_rate_limit, 5000);
+	if (object[epoch] == undefined) object[epoch] = 0;
+	while (object[epoch] >= limit_per_second) await app.sleep(10);
+	object[epoch]++;
 }
 
+let weather_rate_limiter = {};
+let location_rate_limiter = {};
+function clear_rate_limits() {
+	try {
+		let epoch = Math.floor(Date.now() / 1000).toString();
+		clearObject(weather_rate_limiter, epoch);
+		clearObject(location_rate_limiter, epoch);
+	} finally {
+		setTimeout(clear_rate_limits, 5000);
+	}
+}
+function clearObject(object, epoch) {
+	for (let key of Object.keys(object)) {
+		if (key != epoch) {
+			delete object[key];
+		}
+	}
+}
+setTimeout(clear_rate_limits, 5000);
 
 const conversion = {
-"200": {
-"label": "thunderstorm with light rain",
-"icon": "storm-showers"
-},
-"201": {
-"label": "thunderstorm with rain",
-"icon": "storm-showers"
-},
-"202": {
-"label": "thunderstorm with heavy rain",
-"icon": "storm-showers"
-},
-"210": {
-"label": "light thunderstorm",
-"icon": "storm-showers"
-},
-"211": {
-"label": "thunderstorm",
-"icon": "thunderstorm"
-},
-"212": {
-"label": "heavy thunderstorm",
-"icon": "thunderstorm"
-},
-"221": {
-"label": "ragged thunderstorm",
-"icon": "thunderstorm"
-},
-"230": {
-"label": "thunderstorm with light drizzle",
-"icon": "storm-showers"
-},
-"231": {
-"label": "thunderstorm with drizzle",
-"icon": "storm-showers"
-},
-"232": {
-"label": "thunderstorm with heavy drizzle",
-"icon": "storm-showers"
-},
-"300": {
-"label": "light intensity drizzle",
-"icon": "sprinkle"
-},
-"301": {
-"label": "drizzle",
-"icon": "sprinkle"
-},
-"302": {
-"label": "heavy intensity drizzle",
-"icon": "sprinkle"
-},
-"310": {
-"label": "light intensity drizzle rain",
-"icon": "sprinkle"
-},
-"311": {
-"label": "drizzle rain",
-"icon": "sprinkle"
-},
-"312": {
-"label": "heavy intensity drizzle rain",
-"icon": "sprinkle"
-},
-"313": {
-"label": "shower rain and drizzle",
-"icon": "sprinkle"
-},
-"314": {
-"label": "heavy shower rain and drizzle",
-"icon": "sprinkle"
-},
-"321": {
-"label": "shower drizzle",
-"icon": "sprinkle"
-},
-"500": {
-"label": "light rain",
-"icon": "rain"
-},
-"501": {
-"label": "moderate rain",
-"icon": "rain"
-},
-"502": {
-"label": "heavy intensity rain",
-"icon": "rain"
-},
-"503": {
-"label": "very heavy rain",
-"icon": "rain"
-},
-"504": {
-"label": "extreme rain",
-"icon": "rain"
-},
-"511": {
-"label": "freezing rain",
-"icon": "sleet"
-},
-"520": {
-"label": "light intensity shower rain",
-"icon": "showers"
-},
-"521": {
-"label": "shower rain",
-"icon": "showers"
-},
-"522": {
-"label": "heavy intensity shower rain",
-"icon": "showers"
-},
-"531": {
-"label": "ragged shower rain",
-"icon": "showers"
-},
-"600": {
-"label": "light snow",
-"icon": "snow"
-},
-"601": {
-"label": "snow",
-"icon": "snow"
-},
-"602": {
-"label": "heavy snow",
-"icon": "snow"
-},
-"611": {
-"label": "sleet",
-"icon": "sleet"
-},
-"612": {
-"label": "shower sleet",
-"icon": "sleet"
-},
-"615": {
-"label": "light rain and snow",
-"icon": "sleet"
-},
-"616": {
-"label": "rain and snow",
-"icon": "sleet"
-},
-"620": {
-"label": "light shower snow",
-"icon": "sleet"
-},
-"621": {
-"label": "shower snow",
-"icon": "sleet"
-},
-"622": {
-"label": "heavy shower snow",
-"icon": "sleet"
-},
-"701": {
-"label": "mist",
-"icon": "sprinkle"
-},
-"711": {
-"label": "smoke",
-"icon": "smoke"
-},
-"721": {
-"label": "haze",
-"icon": "day-haze"
-},
-"731": {
-"label": "sand, dust whirls",
-"icon": "cloudy-gusts"
-},
-"741": {
-"label": "fog",
-"icon": "fog"
-},
-"751": {
-"label": "sand",
-"icon": "cloudy-gusts"
-},
-"761": {
-"label": "dust",
-"icon": "dust"
-},
-"762": {
-"label": "volcanic ash",
-"icon": "smog"
-},
-"771": {
-"label": "squalls",
-"icon": "day-windy"
-},
-"781": {
-"label": "tornado",
-"icon": "tornado"
-},
-"800": {
-"label": "clear sky",
-"icon": "day_clear"
-},
-"801": {
-"label": "few clouds",
-"icon": "cloudy"
-},
-"802": {
-"label": "scattered clouds",
-"icon": "cloudy"
-},
-"803": {
-"label": "broken clouds",
-"icon": "cloudy"
-},
-"804": {
-"label": "overcast clouds",
-"icon": "cloudy"
-},
-"900": {
-"label": "tornado",
-"icon": "tornado"
-},
-"901": {
-"label": "tropical storm",
-"icon": "hurricane"
-},
-"902": {
-"label": "hurricane",
-"icon": "hurricane"
-},
-"903": {
-"label": "cold",
-"icon": "snowflake-cold"
-},
-"904": {
-"label": "hot",
-"icon": "hot"
-},
-"905": {
-"label": "windy",
-"icon": "windy"
-},
-"906": {
-"label": "hail",
-"icon": "hail"
-},
-"951": {
-"label": "calm",
-"icon": "day_clear"
-},
-"952": {
-"label": "light breeze",
-"icon": "cloudy-gusts"
-},
-"953": {
-"label": "gentle breeze",
-"icon": "cloudy-gusts"
-},
-"954": {
-"label": "moderate breeze",
-"icon": "cloudy-gusts"
-},
-"955": {
-"label": "fresh breeze",
-"icon": "cloudy-gusts"
-},
-"956": {
-"label": "strong breeze",
-"icon": "cloudy-gusts"
-},
-"957": {
-"label": "high wind, near gale",
-"icon": "cloudy-gusts"
-},
-"958": {
-"label": "gale",
-"icon": "cloudy-gusts"
-},
-"959": {
-"label": "severe gale",
-"icon": "cloudy-gusts"
-},
-"960": {
-"label": "storm",
-"icon": "thunderstorm"
-},
-"961": {
-"label": "violent storm",
-"icon": "thunderstorm"
-},
-"962": {
-"label": "hurricane",
-"icon": "cloudy-gusts"
-}
+	"200": {"label": "thunderstorm with light rain","icon": "storm-showers"},
+	"201": {"label": "thunderstorm with rain","icon": "storm-showers"},
+	"202": {"label": "thunderstorm with heavy rain","icon": "storm-showers"},
+	"210": {"label": "light thunderstorm","icon": "storm-showers"},
+	"211": {"label": "thunderstorm","icon": "thunderstorm"},
+	"212": {"label": "heavy thunderstorm","icon": "thunderstorm"},
+	"221": {"label": "ragged thunderstorm","icon": "thunderstorm"},
+	"230": {"label": "thunderstorm with light drizzle","icon": "storm-showers"},
+	"231": {"label": "thunderstorm with drizzle","icon": "storm-showers"},
+	"232": {"label": "thunderstorm with heavy drizzle","icon": "storm-showers"},
+	"300": {"label": "light intensity drizzle","icon": "sprinkle"},
+	"301": {"label": "drizzle","icon": "sprinkle"},
+	"302": {"label": "heavy intensity drizzle","icon": "sprinkle"},
+	"310": {"label": "light intensity drizzle rain","icon": "sprinkle"},
+	"311": {"label": "drizzle rain","icon": "sprinkle"},
+	"312": {"label": "heavy intensity drizzle rain","icon": "sprinkle"},
+	"313": {"label": "shower rain and drizzle","icon": "sprinkle"},
+	"314": {"label": "heavy shower rain and drizzle","icon": "sprinkle"},
+	"321": {"label": "shower drizzle","icon": "sprinkle"},
+	"500": {"label": "light rain","icon": "rain"},
+	"501": {"label": "moderate rain","icon": "rain"},
+	"502": {"label": "heavy intensity rain","icon": "rain"},
+	"503": {"label": "very heavy rain","icon": "rain"},
+	"504": {"label": "extreme rain","icon": "rain"},
+	"511": {"label": "freezing rain","icon": "sleet"},
+	"520": {"label": "light intensity shower rain","icon": "showers"},
+	"521": {"label": "shower rain","icon": "showers"},
+	"522": {"label": "heavy intensity shower rain","icon": "showers"},
+	"531": {"label": "ragged shower rain","icon": "showers"},
+	"600": {"label": "light snow","icon": "snow"},
+	"601": {"label": "snow","icon": "snow"},
+	"602": {"label": "heavy snow","icon": "snow"},
+	"611": {"label": "sleet","icon": "sleet"},
+	"612": {"label": "shower sleet","icon": "sleet"},
+	"615": {"label": "light rain and snow","icon": "sleet"},
+	"616": {"label": "rain and snow","icon": "sleet"},
+	"620": {"label": "light shower snow","icon": "sleet"},
+	"621": {"label": "shower snow","icon": "sleet"},
+	"622": {"label": "heavy shower snow","icon": "sleet"},
+	"701": {"label": "mist","icon": "sprinkle"},
+	"711": {"label": "smoke","icon": "smoke"},
+	"721": {"label": "haze","icon": "day-haze"},
+	"731": {"label": "sand, dust whirls","icon": "cloudy-gusts"},
+	"741": {"label": "fog","icon": "fog"},
+	"751": {"label": "sand","icon": "cloudy-gusts"},
+	"761": {"label": "dust","icon": "dust"},
+	"762": {"label": "volcanic ash","icon": "smog"},
+	"771": {"label": "squalls","icon": "day-windy"},
+	"781": {"label": "tornado","icon": "tornado"},
+	"800": {"label": "clear sky","icon": "day_clear"},
+	"801": {"label": "few clouds","icon": "cloudy"},
+	"802": {"label": "scattered clouds","icon": "cloudy"},
+	"803": {"label": "broken clouds","icon": "cloudy"},
+	"804": {"label": "overcast clouds","icon": "cloudy"},
+	"900": {"label": "tornado","icon": "tornado"},
+	"901": {"label": "tropical storm","icon": "hurricane"},
+	"902": {"label": "hurricane","icon": "hurricane"},
+	"903": {"label": "cold","icon": "snowflake-cold"},
+	"904": {"label": "hot","icon": "hot"},
+	"905": {"label": "windy","icon": "windy"},
+	"906": {"label": "hail","icon": "hail"},
+	"951": {"label": "calm","icon": "day_clear"},
+	"952": {"label": "light breeze","icon": "cloudy-gusts"},
+	"953": {"label": "gentle breeze","icon": "cloudy-gusts"},
+	"954": {"label": "moderate breeze","icon": "cloudy-gusts"},
+	"955": {"label": "fresh breeze","icon": "cloudy-gusts"},
+	"956": {"label": "strong breeze","icon": "cloudy-gusts"},
+	"957": {"label": "high wind, near gale","icon": "cloudy-gusts"},
+	"958": {"label": "gale","icon": "cloudy-gusts"},
+	"959": {"label": "severe gale","icon": "cloudy-gusts"},
+	"960": {"label": "storm","icon": "thunderstorm"},
+	"961": {"label": "violent storm","icon": "thunderstorm"},
+	"962": {"label": "hurricane","icon": "cloudy-gusts"}
 };
